cc0d65240e910d446417c51acc5e478933851323
dylan59400 <dylan.leroy59400@gmail.com>
2022-09-19 11:34:02 +0200

    exercice

---
+++ script.js
@@ -0,0 +1,35 @@
+var ville= "Cambrai";
+const hypotenuse= 8;
+var admission= true;
+
+var nom="Leroy";
+var prenom="Dylan";
+var age= 24;
+
+var pommes = 4;
+var pêches = 6;
+var totalFruits = pommes * pêches;
+
+console.log(totalFruits);
+
+var diviseur = 25;
+var dividende = 5;
+var resultat = diviseur / dividende;
+
+console.log(resultat);
+
+function addition(a, b) {
+    console.log(a);
+    console.log(b);
+}
+
+addition(4 + 8);
+addition(4 - 8);
+addition(4 * 8);
+addition(4 / 8);
+
+const person = {
+    firstName: "Dylan",
+    lastname: "Leroy",
+
+};
